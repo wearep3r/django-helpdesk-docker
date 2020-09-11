dac66f103af9f34e301bc1769ad70ea06280e57c
Fabian Peter <fabian@peter.saarland>
2020-09-11 13:59:23 +0200

    fix: media permissions

--- cron.sh
+++ cron.sh
@@ -1,5 +1,11 @@
 while true; 
 do 
+  # Collecting mails every minute
   python /app/manage.py get_email; 
+
+  # Correcting volume permissions
+  # files are served by nginx in separate container
+  # user nginx has id 101
+  chown 101:101 /app/media -R
   sleep 60; 
 done
