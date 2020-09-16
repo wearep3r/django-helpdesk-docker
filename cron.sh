432d2bd267e312b03c53b51c98a5110f3800add5
Fabian Peter <fabian@peter.saarland>
2020-09-16 11:56:49 +0200

    fix: cronjob: escalations

--- cron.sh
+++ cron.sh
@@ -7,5 +7,8 @@ do
   # files are served by nginx in separate container
   # user nginx has id 101
   chown 101:101 /app/media -R
+
+  # Setting escalations
+  /app/manage.py create_escalation_exclusions --days saturday,sunday
   sleep 60; 
 done
